c271eb4c052bf2c69cf78e48f1f1b99554639541
frunkyfrunk <verhagen.frank@gmail.com>
2018-11-26 11:17:49 +0100

    added gitignore files

--- Backend/Backend/Controllers/ValuesController.cs
+++ Backend/Backend/Controllers/ValuesController.cs
@@ -10,11 +10,7 @@ namespace Backend.Controllers
     public class ValuesController : Controller
     {
         // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+      
 
         // GET api/values/5
         [HttpGet("{id}")]
