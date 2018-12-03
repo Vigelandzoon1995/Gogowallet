e5f8f82222715fdcfd42c0f0d98b18aca5baf161
Benny <bennymohan08@gmail.com>
2018-12-03 18:33:36 +0100

    Add GoogleMap

--- src/pages/track/track.ts
+++ src/pages/track/track.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
 
 /**
  * Generated class for the TrackPage page.
@@ -14,12 +24,47 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'track.html',
 })
 export class TrackPage {
-
+  map: GoogleMap;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TrackPage');
+    this.loadMap();
+  }
+  loadMap() {
+
+    // This code is necessary for browser
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyCkJEZtkCK1urLuwBRsooqttYDNIzsOBf4',
+      'API_KEY_FOR_BROWSER_DEBUG': ''
+    });
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: 43.0741904,
+           lng: -89.3809802
+         },
+         zoom: 18,
+         tilt: 30
+       }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 43.0741904,
+        lng: -89.3809802
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
   }
 
 }
