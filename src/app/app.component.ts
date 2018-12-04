814d637c1a7750eafbd963a75df66711a92a4b69
Johan Bos <bosbosjohan@gmail.com>
2018-12-04 12:21:11 +0100

    - Hide standard splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      splashScreen.hide();
+      //splashScreen.hide();
 
       timer(3000).subscribe(() => this.showSplash = false)
     });
