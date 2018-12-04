0b88ff7f8fa8e0e5968e724e87f6c48f131f9366
agentmushroom <43029598+agentmushroom@users.noreply.github.com>
2018-12-04 10:51:38 +0100

    Finished registration page
    Finished registration page
    
    Co-Authored-By: Johan Bos <jhnbos@users.noreply.github.com>

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 import {TabsPage} from '../tabs/tabs';
+import User from '../../services/models/user.model';
 
 /**
  * Generated class for the SignupPage page.
@@ -15,6 +16,8 @@ import {TabsPage} from '../tabs/tabs';
   templateUrl: 'signup.html',
 })
 export class SignupPage {
+  user: User = new User();
+  password_confirm: string;
 
   constructor(private navCtrl: NavController) {
   }
@@ -22,7 +25,12 @@ export class SignupPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
   }
-  signup(){
-    this.navCtrl.push(TabsPage);
+  
+  register() {
+    if(this.user.password == this.password_confirm){
+      alert('Succes'); 
+    }else{
+      alert('Failed')
+    }
   }
 }
---
+++ src/services/models/user.model.ts
@@ -0,0 +1,11 @@
+export default class User {
+    user_id: number = null;
+    first_name: string = null;
+    last_name: string = null;
+    email: string = null;
+    password: string = null;
+    pin_code: number = null;
+    budget: number = null;
+    bank_account: string = null;
+    profile_picture: string = null;
+}
