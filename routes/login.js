dd5d13549508e30b7c79fc5561147a2053d990e2
frunkyfrunk <verhagen.frank@gmail.com>
2018-12-13 11:43:18 +0100

    Adjusted everything to Johan's database structure

--- routes/login.js
+++ routes/login.js
@@ -3,19 +3,14 @@ var jwt = require('jsonwebtoken');
 var router = express.Router();
 
 const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
-const someOtherPlaintextPassword = 'not_bacon';
-const hashed = '';
 
 router.post('/', function (req, res) {
-  var username = req.body.username;
-  db.query("SELECT * from Users where Username = ?",
-    [username],
+  var email = req.body.email;
+  db.query('SELECT * from users u INNER JOIN user_passwords p ON u.user_id = p.user_id where email = ?;',
+    [email],
     function (error, results, fields) {
-      console.log(results)
-      if (results.length != 0 && results[0].Password != null && results[0].ID != null) {
-        bcrypt.compare(req.body.password, results[0].Password, function (err, result) {
+      if (results.length != 0 && results[0].password != null && results[0].user_id != null) {
+        bcrypt.compare(req.body.password, results[0].password, function (err, result) {
           if (result) {
             var token = jwt.sign({ id: results[0].ID }, "secretkey");
             res.json({
--- routes/register.js
+++ routes/register.js
@@ -8,24 +8,43 @@ router.post('/', function (req, res) {
     if (req.body.password != null &&
         req.body.password.length > 10) {
         bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-            var username = req.body.username;
-            db.query("INSERT INTO Users (Username, Password) SELECT ?, ? FROM DUAL WHERE NOT EXISTS (SELECT Username FROM Users WHERE Username=?)",
-                [username, hash, username],
+            var email = req.body.email;
+            var first_name = req.body.first_name;
+            var last_name = req.body.last_name;
+            console.log(req.body)
+            db.query('INSERT INTO users (email, first_name, last_name) SELECT ?, ?, ? FROM DUAL WHERE NOT EXISTS (SELECT email FROM users WHERE email=?);',
+                [email, first_name, last_name, email],
                 function (error, results, fields) {
                     if (results != null && results.affectedRows == 1) {
-                        res.json({
-                            success: true
-                        })
+                        db.query('INSERT INTO user_passwords (user_id, password) VALUES((SELECT user_id FROM users WHERE email=?), ?)',
+                            [email, hash],
+                            function (error, results, fields) {
+                                if (results != null && results.affectedRows == 1) {
+                                    res.json({
+                                        success: true
+                                    })
+                                } else {
+                                    console.log(error)
+                                    res.json({
+                                        success: false
+                                    })
+                                }
+                            });
                     } else {
+                        console.log(error)
                         res.json({
                             success: false
                         })
                     }
-                });
+                },
+            );
         });
+    } else {
+        console.log(error)
+        res.json({
+            success: false
+        })
     }
-
-
 });
 
 module.exports = router;
