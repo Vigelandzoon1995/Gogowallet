c8e53ffba7694dd8904e7500e383c161908471a1
frunkyfrunk <verhagen.frank@gmail.com>
2018-12-14 11:59:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ global.db = db;
 var http = require('http');
 
 //If the command is ran using a jasmine instance, use the test port 4444
+//So It won't disturb the pm2 instance running on 3333
 if (process.argv[1].includes("jasmine.js")) {
   http.createServer(app).listen(4444, "0.0.0.0");
   console.log("running jasmine test environment at port 4444")
