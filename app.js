208e35aa29a8c6091240f920a3b0813ece0e5776
Johan Bos <bosbosjohan@gmail.com>
2018-12-15 12:05:36 +0100

    Changed to port 80

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var cors = require('cors')
+var cors = require('cors');
 
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -35,16 +35,16 @@ global.db = db;
 var http = require('http');
 
 //If the command is ran using a jasmine instance, use the test port 4444
-//So It won't disturb the pm2 instance running on 3333
+//So It won't disturb the pm2 instance running on 80
 if (process.argv[1].includes("jasmine.js")) {
   http.createServer(app).listen(4444, "0.0.0.0");
-  console.log("running jasmine test environment at port 4444")
+  console.log("running jasmine test environment at port 4444");
 } else {
-  http.createServer(app).listen(3333, "0.0.0.0");
-  console.log("running at port 3333")
+  http.createServer(app).listen(80, "0.0.0.0");
+  console.log("running at port 80");
 }
 
-app.use(cors())
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -62,7 +62,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
-app.use('/register', registerRouter)
+app.use('/register', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
