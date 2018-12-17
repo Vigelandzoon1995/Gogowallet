bf2b84b33c04b62ccb99e10b029cee8b3487bbaf
frunkyfrunk <verhagen.frank@gmail.com>
2018-12-17 12:02:45 +0100

    Added new test for registration & returning user object when logging in

--- routes/register.js
+++ routes/register.js
@@ -4,48 +4,59 @@ var router = express.Router();
 const bcrypt = require('bcrypt');
 const saltRounds = 14;
 
-router.post('/', function (req, res) {
+router.post('/', async function (req, res) {
     if (req.body.password != null &&
         req.body.password.length >= 8 &&
         /^.*(?=.{10,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/.test(req.body.password)) {
-        bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-            var email = req.body.email;
-            var first_name = req.body.first_name;
-            var last_name = req.body.last_name;
-            db.query('INSERT INTO users (email, first_name, last_name) SELECT ?, ?, ? FROM DUAL WHERE NOT EXISTS (SELECT email FROM users WHERE email=?);',
-                [email, first_name, last_name, email],
-                function (error, results, fields) {
-                    if (results != null && results.affectedRows == 1) {
-                        db.query('INSERT INTO user_passwords (user_id, password) VALUES((SELECT user_id FROM users WHERE email=?), ?)',
-                            [email, hash],
-                            function (error, results, fields) {
-                                if (results != null && results.affectedRows == 1) {
-                                    res.json({
-                                        success: true
-                                    })
-                                } else {
-                                    console.log(error)
-                                    res.json({
-                                        success: false
-                                    })
-                                }
-                            });
-                    } else {
-                        console.log(error)
-                        res.json({
-                            success: false
-                        })
-                    }
-                },
-            );
-        });
+        var hash = await hashPassword(req.body.password)
+        var email = req.body.email;
+        var first_name = req.body.first_name;
+        var last_name = req.body.last_name;
+        db.query('INSERT INTO users (email, first_name, last_name) SELECT ?, ?, ? FROM DUAL WHERE NOT EXISTS (SELECT email FROM users WHERE email=?);',
+            [email, first_name, last_name, email],
+            function (error, results, fields) {
+                if (results != null && results.affectedRows == 1) {
+                    db.query('INSERT INTO user_passwords (user_id, password) VALUES((SELECT user_id FROM users WHERE email=?), ?)',
+                        [email, hash],
+                        function (error, results, fields) {
+                            if (results != null && results.affectedRows == 1) {
+                                res.json({
+                                    success: true
+                                })
+                            } else {
+                                console.log(error)
+                                res.json({
+                                    success: false
+                                })
+                            }
+                        });
+                } else {
+                    console.log(error)
+                    res.json({
+                        success: false
+                    })
+                }
+            },
+        );
     } else {
-        console.log("")
         res.json({
             success: false
         })
     }
 });
+
+async function hashPassword(password) {
+    const saltRounds = 14;
+
+    const hashedPassword = await new Promise((resolve, reject) => {
+        bcrypt.hash(password, saltRounds, function (err, hash) {
+            if (err) reject(err)
+            resolve(hash)
+        });
+    })
+
+    return hashedPassword
+}
 
 module.exports = router;
 
--- spec/register.spec.js
+++ spec/register.spec.js
@@ -2,14 +2,34 @@ describe("registration", () => {
     var register;
     beforeAll(() => {
         register = require("../routes/register");
+        //make a mock db object instead of a real database connection to test the isolated express unit
+        //this will always return affectedRows: 1
+        global.db = {
+            query: function (query, variables, succeed) {
+                succeed("", { affectedRows: 1 })
+            }
+        };
+        register.saltRounds = "";
     });
     describe("Test regex", () => {
         var data = {};
         beforeAll((done) => {
             done();
         });
-        it("To fail", () => {
-            register.stack[0].route.stack[0].handle({ body: {email:"", password: "sdjalfkjdfsdfslkfjlksf"} }, { json: function (response) { expect(response.success).toBe(false)} })
+        it("To succeed", async () => {
+            await register.stack[0].route.stack[0].handle({ body: { email: "test@test.nl", password: "sdjalfkjdfsdfslkfjlksf##dDD33" } }, { json: function (response) { expect(response.success).toBe(true) } })
+        });
+        it("To fail", async () => {
+            await register.stack[0].route.stack[0].handle({ body: { email: "test@test.nl", password: "" } }, { json: function (response) { expect(response.success).toBe(false) } })
+        });
+        it("To fail", async () => {
+            await register.stack[0].route.stack[0].handle({ body: { email: "", password: "" } }, { json: function (response) { expect(response.success).toBe(false) } })
+        });
+        it("To fail", async () => {
+            await register.stack[0].route.stack[0].handle({ body: { email: "test@test.nl", password: "" } }, { json: function (response) { expect(response.success).toBe(false) } })
+        });
+        it("To fail", async () => {
+            await register.stack[0].route.stack[0].handle({ body: { email: "test@test.nl", password: "sdjalfkjdfsdfslkfjlksf##dDD" } }, { json: function (response) { expect(response.success).toBe(false) } })
         });
     });
 });
