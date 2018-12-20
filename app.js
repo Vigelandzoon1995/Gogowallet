4818fb49b358653d92506202776ceea621a3c4a8
sjaakslingerkaak <maartenf93@gmail.com>
2018-12-20 11:57:59 +0100

    gps route added to app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var registerRouter = require('./routes/register');
+var gpsRouter = require('./routes/gps');
 
 
 var app = express();
@@ -63,6 +64,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
+app.use('/gps', gpsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
