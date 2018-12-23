0486b4fb65c83ee3fc38027b475d5fbc25452831
Johan Bos <9801560+JHNBOS@users.noreply.github.com>
2018-12-23 17:33:04 +0100

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -39,5 +39,41 @@ router.post('/', function (req, res) {
     });
 });
 
+router.post('/pin', function (req, res) {
+  var email = req.body.email;
+  var pin = req.body.pin_code;
+  db.query('SELECT * from users u INNER JOIN user_passwords p ON u.user_id = p.user_id where email = ? AND pin_code = ?;',
+    [email, pin],
+    function (error, results, fields) {
+      if (results.length != 0 && results[0].password != null && results[0].pin_code != null && results[0].user_id != null) {
+        if(req.body.password == results[0].password) {
+          if (result) {
+            var token = jwt.sign({ id: results[0].user_id }, "secretkey");
+            res.json({
+              token: token,
+              user: {
+                user_id: results[0].user_id,
+                first_name: results[0].first_name,
+                last_name: results[0].last_name,
+                bank_account: results[0].bank_account,
+                email: results[0].email,
+                password: results[0].password,
+                profile_picture: results[0].profile_picture
+              }
+            })
+          } else {
+            res.json({
+              token: false
+            })
+          }
+        }
+      } else {
+        res.json({
+          token: false
+        })
+      }
+    });
+});
+
 module.exports = router;
 
