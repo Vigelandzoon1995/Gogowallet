a37c35a6c125f2526290409b0a27ef45d0a921c1
Johan Bos <bosbosjohan@gmail.com>
2018-12-27 13:38:43 +0100

    Finished PIN logic for sign in

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -7,6 +7,7 @@ import User from '../../shared/models/user.model';
 import { ResetPasswordPage } from '../reset-password/reset-password';
 import { SignupPage } from '../signup/signup';
 import { TabsPage } from '../tabs/tabs';
+import { CustomValidators } from '../../shared/helpers/custom-validators';
 
 @IonicPage()
 @Component({
@@ -16,34 +17,48 @@ import { TabsPage } from '../tabs/tabs';
 export class SigninPage {
   currentUser: User = null;
   loginForm: FormGroup;
+  usePinForm: boolean = false;
+  usePin: boolean = false;
   emailInput: string;
   passwordInput: string;
   pinCodeInput: string;
 
   constructor(private navCtrl: NavController, private formBuilder: FormBuilder, public alertCtrl: AlertController, private authService: AuthenticationService,
     private storage: Storage) {
-    this.createFormGroup();
-    this.checkLocalUser();
+  }
+
+  ionViewCanEnter() {
+    this.init();
   }
 
   ionViewDidLoad() {
+  }
+
+  init() {
+    this.createFormGroup();
+    this.checkLocalUser();
   }
 
   createFormGroup() {
     this.loginForm = this.formBuilder.group({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required]),
-      pin: new FormControl('', []),
+      pinpass: new FormControl('', []),
     });
   }
 
   signIn() {
     //this.navCtrl.push(TabsPage);
-    this.authService.signIn(this.emailInput, this.passwordInput);
-  }
 
-  signInByPin() {
-    this.authService.signInByPin(this.currentUser.email, this.currentUser.password, this.pinCodeInput);
+    if (this.usePinForm == true) {
+      if (this.usePin == true) {
+        this.authService.signInByPin(this.currentUser.email, this.currentUser.password, this.pinCodeInput);
+      } else {
+        this.authService.signIn(this.currentUser.email, this.pinCodeInput);
+      }
+    } else {
+      this.authService.signIn(this.emailInput, this.passwordInput);
+    }
   }
 
   checkLocalUser() {
@@ -51,15 +66,35 @@ export class SigninPage {
       (result) => {
         if (result != null) {
           this.currentUser = result;
-
-          // Remove email and password controls
-          this.loginForm.removeControl('email');
-          this.loginForm.removeControl('password');
-
-          // Change pin control to required
-          this.loginForm.controls['pin'].setValidators([Validators.required, Validators.minLength(4), Validators.maxLength(10), Validators.pattern(/^[0-9]*$/)]);
+          this.usePIN();
         }
       }
+    );
+  }
+
+  usePIN() {
+    this.usePinForm = true;
+    this.usePin = true;
+
+    // Remove email and password controls
+    // Change pin control to required
+    this.loginForm.removeControl('email');
+    this.loginForm.removeControl('password');
+    this.loginForm.controls['pinpass'].setValidators([Validators.required, Validators.minLength(4), Validators.maxLength(10), Validators.pattern(/^[0-9]*$/)]);
+  }
+
+  usePassword() {
+    this.usePinForm = true;
+    this.usePin = false;
+
+    // Reset pin control
+    // Add password validators
+    this.loginForm.controls['pinpass'].clearValidators();
+    this.loginForm.controls['pinpass'].setValidators(
+      [
+        Validators.required,
+        CustomValidators.patternValidator(/^.*(?=.{10,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/, { hasPassed: true })
+      ]
     );
   }
 
