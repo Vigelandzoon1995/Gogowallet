00f222dacb2aa57894964a887ba7e97ec560a665
Johan Bos <bosbosjohan@gmail.com>
2019-01-08 17:47:11 +0100

    Remove clear auth token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,64 +9,61 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { AuthenticationService } from '../shared/helpers/auth.service';
 
 @Component({
-  templateUrl: 'app.html'
+	templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild('mainNav') nav: NavController;
-  rootPage: any = SigninPage;
+	@ViewChild('mainNav') nav: NavController;
+	rootPage: any = SigninPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, private splashScreen: SplashScreen, private androidPermissions: AndroidPermissions,
-    private localNotifications: LocalNotifications, private authService: AuthenticationService) {
-    //Subscribe to events of Authentication Service
-    this.authService.logoutEvent.subscribe(
-      res => {
-        this.nav.push(SigninPage);
-      }
-    );
+	constructor(platform: Platform, statusBar: StatusBar, private splashScreen: SplashScreen, private androidPermissions: AndroidPermissions,
+		private localNotifications: LocalNotifications, private authService: AuthenticationService) {
+		//Subscribe to events of Authentication Service
+		this.authService.logoutEvent.subscribe(
+			res => {
+				this.nav.push(SigninPage);
+			}
+		);
 
-    this.authService.loginEvent.subscribe(
-      res => {
-        this.nav.push(TabsPage);
-      }
-    );
+		this.authService.loginEvent.subscribe(
+			res => {
+				this.nav.push(TabsPage);
+			}
+		);
 
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      this.splashScreen.hide();
+		platform.ready().then(() => {
+			// Okay, so the platform is ready and our plugins are available.
+			// Here you can do any higher level native things you might need.
+			statusBar.styleDefault();
+			this.splashScreen.hide();
 
-      // Clear all local storage
-      this.authService.clear();
+			this.askPermissions();
+		});
+	}
 
-      this.askPermissions();
-    });
-  }
-
-  checkPermissions() {
-    let hasPermissions = true;
-    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.READ_EXTERNAL_STORAGE).then((result) => { if (!result.hasPermission) { hasPermissions = false; } });
-    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE).then((result) => { if (!result.hasPermission) { hasPermissions = false; } });
-    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.VIBRATE).then((result) => { if (!result.hasPermission) { hasPermissions = false; } });
-    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION).then((result) => { if (!result.hasPermission) { hasPermissions = false; } });
-    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.ACCESS_FINE_LOCATION).then((result) => { if (!result.hasPermission) { hasPermissions = false; } });
-    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.ACCESS_LOCATION_EXTRA_COMMANDS).then((result) => { if (!result.hasPermission) { hasPermissions = false; } });
+	checkPermissions() {
+		let hasPermissions = true;
+		this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.READ_EXTERNAL_STORAGE).then((result) => { if (!result.hasPermission) { hasPermissions = false; } });
+		this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE).then((result) => { if (!result.hasPermission) { hasPermissions = false; } });
+		this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.VIBRATE).then((result) => { if (!result.hasPermission) { hasPermissions = false; } });
+		this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION).then((result) => { if (!result.hasPermission) { hasPermissions = false; } });
+		this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.ACCESS_FINE_LOCATION).then((result) => { if (!result.hasPermission) { hasPermissions = false; } });
+		this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.ACCESS_LOCATION_EXTRA_COMMANDS).then((result) => { if (!result.hasPermission) { hasPermissions = false; } });
 
 
-    if (!this.localNotifications.hasPermission) hasPermissions = false;
-    if (!hasPermissions) this.askPermissions();
-  }
+		if (!this.localNotifications.hasPermission) hasPermissions = false;
+		if (!hasPermissions) this.askPermissions();
+	}
 
-  askPermissions() {
-    this.androidPermissions.requestPermissions(
-      [
-        this.androidPermissions.PERMISSION.READ_EXTERNAL_STORAGE,
-        this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE,
-        this.androidPermissions.PERMISSION.VIBRATE,
-        this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION,
-        this.androidPermissions.PERMISSION.ACCESS_FINE_LOCATION,
-        this.androidPermissions.PERMISSION.ACCESS_LOCATION_EXTRA_COMMANDS
-      ]);
-    this.localNotifications.requestPermission();
-  }
+	askPermissions() {
+		this.androidPermissions.requestPermissions(
+			[
+				this.androidPermissions.PERMISSION.READ_EXTERNAL_STORAGE,
+				this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE,
+				this.androidPermissions.PERMISSION.VIBRATE,
+				this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION,
+				this.androidPermissions.PERMISSION.ACCESS_FINE_LOCATION,
+				this.androidPermissions.PERMISSION.ACCESS_LOCATION_EXTRA_COMMANDS
+			]);
+		this.localNotifications.requestPermission();
+	}
 }
