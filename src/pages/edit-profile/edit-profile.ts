78558d018ec9ee46b95b74dd467b181ec0e895d9
Johan Bos <bosbosjohan@gmail.com>
2019-01-08 21:02:17 +0100

    Show error when validation errors occur

--- src/pages/edit-profile/edit-profile.ts
+++ src/pages/edit-profile/edit-profile.ts
@@ -12,94 +12,94 @@ import { UserService } from '../../shared/services/user.service';
 
 @IonicPage()
 @Component({
-  selector: 'page-edit-profile',
-  templateUrl: 'edit-profile.html',
+	selector: 'page-edit-profile',
+	templateUrl: 'edit-profile.html',
 })
 export class EditProfilePage {
-  profileForm: FormGroup;
-  user: User;
-  base64Image: string;
+	profileForm: FormGroup;
+	user: User;
+	base64Image: string;
 
-  clearStorage: boolean = false;
-  password: string = null;
-  newPassword: string = null;
+	clearStorage: boolean = false;
+	password: string = null;
+	newPassword: string = null;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder, private camera: Camera,
-    private userService: UserService, private authService: AuthenticationService, private storage: Storage, private loadingService: LoadingService) {
-    this.createFormGroup();
-    this.getUser();
-  }
+	constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder, private camera: Camera,
+		private userService: UserService, private authService: AuthenticationService, private storage: Storage, private loadingService: LoadingService) {
+		this.createFormGroup();
+		this.getUser();
+	}
 
-  ionViewDidLoad() {
-  }
+	ionViewDidLoad() {
+	}
 
-  createFormGroup() {
-    this.profileForm = this.formBuilder.group({
-      email: new FormControl('', Validators.compose([Validators.required, Validators.email])),
-      first_name: new FormControl('', Validators.compose([Validators.required, Validators.pattern(/[a-zA-Z0-9\.\-\_\ ]+(?!.*[\.\-\_]{4,})$/gm)])),
-      last_name: new FormControl('', Validators.compose([Validators.required, Validators.pattern(/[a-zA-Z0-9\.\-\_\ ]+(?!.*[\.\-\_]{4,})$/gm)])),
-      bank_account: new FormControl('', Validators.compose([Validators.pattern(/^[a-zA-Z0-9]+$/)])),
-      pin: new FormControl('', Validators.compose([Validators.pattern(/^[0-9]*$/)])),
-      password: new FormControl('', [
-        CustomValidators.patternValidator(/^.*(?=.{10,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/, { hasPassed: true }),
-      ]),
-      new_password: new FormControl('', [
-        CustomValidators.patternValidator(/^.*(?=.{10,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/, { hasPassed: true }),
-      ])
-    });
-  }
+	createFormGroup() {
+		this.profileForm = this.formBuilder.group({
+			email: new FormControl('', Validators.compose([Validators.required, Validators.email])),
+			first_name: new FormControl('', Validators.compose([Validators.required, Validators.pattern(/[a-zA-Z0-9\.\-\_\ ]+(?!.*[\.\-\_]{4,})$/gm)])),
+			last_name: new FormControl('', Validators.compose([Validators.required, Validators.pattern(/[a-zA-Z0-9\.\-\_\ ]+(?!.*[\.\-\_]{4,})$/gm)])),
+			bank_account: new FormControl('', Validators.compose([Validators.pattern(/^[a-zA-Z0-9]+$/)])),
+			pin: new FormControl('', Validators.compose([Validators.pattern(/^[0-9]*$/)])),
+			password: new FormControl('', [
+				CustomValidators.patternValidator(/^.*(?=.{10,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/, { hasPassed: true }),
+			]),
+			new_password: new FormControl('', [
+				CustomValidators.patternValidator(/^.*(?=.{10,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/, { hasPassed: true }),
+			])
+		});
+	}
 
-  getUser() {
-    this.storage.get('currentUser').then(
-      (response) => this.user = response
-    );
-  }
+	getUser() {
+		this.storage.get('currentUser').then(
+			(response) => this.user = response
+		);
+	}
 
-  submit() {
-    let newUser = this.user;
-    newUser.password = null;
+	submit() {
+		let newUser = this.user;
+		newUser.password = null;
 
-    // Show spinner
-    this.loadingService.show();
+		// Show spinner
+		this.loadingService.show();
 
-    // Check if password has changed
-    if (this.password != null && this.newPassword != null) {
-      if (this.password != this.newPassword) {
-        newUser.password = this.newPassword;
-      }
-    }
+		// Check if password has changed
+		if (this.password != null && this.newPassword != null) {
+			if (this.password != this.newPassword) {
+				newUser.password = this.newPassword;
+			}
+		}
 
-    this.userService.update(newUser).subscribe(
-      (response) => {
-        this.authService.removeUser(false);
-        this.authService.saveUser(newUser, false);
+		this.userService.update(newUser).subscribe(
+			(response) => {
+				this.authService.removeUser(false);
+				this.authService.saveUser(newUser, false);
 
-        // Hide spinner
-        this.loadingService.hide();
+				// Hide spinner
+				this.loadingService.hide();
 
-        this.navCtrl.pop();
-      },
-      (error) => {
-        // Hide spinner
-        this.loadingService.hide();
-        Observable.throw(error);
-      }
-    );
-  }
+				this.navCtrl.pop();
+			},
+			(error) => {
+				// Hide spinner
+				this.loadingService.hide();
+				Observable.throw(error);
+			}
+		);
+	}
 
-  uploadImage() {
-    const options: CameraOptions = {
-      quality: 50, // picture quality
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
-    };
+	uploadImage() {
+		const options: CameraOptions = {
+			quality: 50, // picture quality
+			destinationType: this.camera.DestinationType.DATA_URL,
+			encodingType: this.camera.EncodingType.JPEG,
+			sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+		};
 
-    this.camera.getPicture(options).then((imageData) => {
-      this.base64Image = "data:image/jpeg;base64," + imageData;
-      this.user.profile_picture = this.base64Image;
-    }, (error) => {
-      Observable.throw(error);
-    });
-  }
+		this.camera.getPicture(options).then((imageData) => {
+			this.base64Image = "data:image/jpeg;base64," + imageData;
+			this.user.profile_picture = this.base64Image;
+		}, (error) => {
+			Observable.throw(error);
+		});
+	}
 }
