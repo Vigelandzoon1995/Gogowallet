d66eed8856acc16484992d92941153f5627e3af3
Johan Bos <9801560+JHNBOS@users.noreply.github.com>
2019-01-11 12:28:01 +0100

    Update coordinate.js

--- routes/coordinate.js
+++ routes/coordinate.js
@@ -10,13 +10,12 @@ router.post('/create', auth.verifyToken, function (req, res) {
 
 	db.query("SELECT * FROM devices WHERE id=?",
 		[device_id],
-		function(err, result){
-			if(result[0] != undefined && result[0].user_id == res.locals.user_id){
+		function (err, result) {
+			if (result[0] != undefined && result[0].user_id == res.locals.user_id) {
 				db.query("INSERT INTO gps (latitude, longtitude, time, device_id) VALUES (?, ?, ?, ?)", [lat, long, time, device_id], function (err, result) {
 					if (err) {
 						throw err;
 					}
-					console.log('GPS inserted.');
 					if (result) {
 						res.json({
 							response: 'Gps inserted'
@@ -33,16 +32,17 @@ router.post('/create', auth.verifyToken, function (req, res) {
 				})
 			}
 		})
-	
+
 })
 
-/*router.get('/getAll', auth.verifyToken, function (req, res) {
+router.get('/getAll', auth.verifyToken, function (req, res) {
 	//Take the user id directly from the jwt middleware to ensure the user only can get the coordinates of his own device
-	var user_id = res.locals.user_id
-	db.query('SELECT *',
+	var user_id = res.locals.user_id;
+	db.query('SELECT id, latitude, longtitude, time, device_id FROM gps AS g, devices AS d WHERE d.user_id = ? AND g.device_id = d.device_id ORDER BY CONVERT(DATE, g.time) DESC',
+		[user_id],
 		function (error, results, fields) {
 			res.send(results);
 		});
-})*/
+})
 
 module.exports = router;
