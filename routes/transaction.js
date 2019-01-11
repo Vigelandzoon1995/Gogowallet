eacd5c32552142b25a431ea169dde7b8d62f919f
Johan Bos <9801560+JHNBOS@users.noreply.github.com>
2019-01-11 12:14:06 +0100

    Update transaction.js

--- routes/transaction.js
+++ routes/transaction.js
@@ -25,50 +25,48 @@ router.post('/create', auth.verifyToken, function (req, res) {
 })
 
 router.get('/getById', auth.verifyToken, function (req, res) {
-    var user_id = res.locals.user_id
     var id = req.query.id
-    db.query("SELECT * FROM transactions WHERE user_id=? AND id=?",
-        [user_id, id],
+    db.query("SELECT * FROM transactions WHERE id=?",
+        [id],
         function (error, results) {
             res.send(results);
         })
 })
 
 router.get('/getToday', auth.verifyToken, function (req, res) {
-    var user_id = res.locals.user_id
-    db.query("SELECT * FROM transactions WHERE user_id=? AND DATE_FORMAT(date, '%Y-%m-%d')=CURDATE()",
-        [user_id],
+    var bank_account = req.body.bank_account
+    db.query("SELECT * FROM transactions WHERE bank_account=? AND DATE_FORMAT(date, '%Y-%m-%d')=CURDATE()",
+        [bank_account],
         function (error, results) {
             res.send(results);
         })
 })
 
 router.get('/getSince', auth.verifyToken, function (req, res) {
-    var user_id = res.locals.user_id
+    var bank_account = req.body.bank_account
     var date = req.query.date
-    db.query("SELECT * FROM transactions WHERE user_id=? AND DATE_FORMAT(date, '%Y-%m-%d') >= DATE_FORMAT(?, '%Y-%m-%d')",
-        [user_id, date],
+    db.query("SELECT * FROM transactions WHERE bank_account=? AND DATE_FORMAT(date, '%Y-%m-%d') >= DATE_FORMAT(?, '%Y-%m-%d')",
+        [bank_account, date],
         function (error, results) {
             res.send(results);
         })
 })
 
 router.get('/getBetweenDates', auth.verifyToken, function (req, res) {
-    var user_id = res.locals.user_id
+    var bank_account = req.body.bank_account
     var start = req.query.start
     var end = req.query.end
-    db.query("SELECT * FROM transactions WHERE user_id=? AND DATE_FORMAT(date, '%Y-%m-%d') >= DATE_FORMAT(?, '%Y-%m-%d') AND DATE_FORMAT(date, '%Y-%m-%d') <= DATE_FORMAT(?, '%Y-%m-%d')",
-        [user_id, start, end],
+    db.query("SELECT * FROM transactions WHERE bank_account=? AND DATE_FORMAT(date, '%Y-%m-%d') >= DATE_FORMAT(?, '%Y-%m-%d') AND DATE_FORMAT(date, '%Y-%m-%d') <= DATE_FORMAT(?, '%Y-%m-%d')",
+        [bank_account, start, end],
         function (error, results) {
             res.send(results);
         })
 })
 
 router.get('/delete', auth.verifyToken, function (req, res) {
-    var user_id = res.locals.user_id
     var id = req.query.id
-    db.query('DELETE from transactions WHERE user_id=? AND id=?',
-        [user_id, id],
+    db.query('DELETE from transactions WHERE id=?',
+        [id],
         function (error, results, fields) {
             if (results != null && results.affectedRows == 1) {
                 res.json({
