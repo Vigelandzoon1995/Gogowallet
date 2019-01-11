b45c45afd0b01c5f219cdac69c498fc314c05015
Johan Bos <9801560+JHNBOS@users.noreply.github.com>
2019-01-11 12:42:16 +0100

    Update transaction.js

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,59 +1,83 @@
 var express = require('express');
-var auth = require('../modules/auth');
+var auth = require('../modules/auth')
 var router = express.Router();
 
 router.post('/create', auth.verifyToken, function (req, res) {
-	var lat = req.body.lat;
-	var long = req.body.long;
-	var time = req.body.time;
-	var device_id = req.body.device_id;
-
-	db.query("SELECT * FROM devices WHERE id=?",
-		[device_id],
-		function (err, result) {
-			if (result[0] != undefined && result[0].user_id == res.locals.user_id) {
-				db.query("INSERT INTO gps (latitude, longtitude, time, device_id) VALUES (?, ?, ?, ?)", [lat, long, time, device_id], function (err, result) {
-					if (err) {
-						throw err;
-					}
-					if (result) {
-						res.json({
-							response: 'Gps inserted'
-						})
-					} else {
-						res.json({
-							response: 'Error when inserting gps'
-						})
-					}
-				});
-			} else {
-				res.json({
-					response: 'Error when inserting gps'
-				})
-			}
-		})
+    var user_id = res.locals.user_id
+    var name = req.body.name
+    var amount = req.body.amount
+    var date = req.body.date
+    var bank_account = req.body.bank_account
+    db.query("INSERT INTO transactions (user_id, name, amount, date, bank_account) VALUES (?,?,?,?,?)",
+        [user_id, name, amount, date, bank_account],
+        function (error, results) {
+            if (results != null && results.affectedRows == 1) {
+                res.json({
+                    success: true
+                })
+            } else {
+                console.log(error)
+                res.json({
+                    success: false
+                })
+            }
+        });
 })
 
-router.get('/getAll', auth.verifyToken, function (req, res) {
-	//Take the user id directly from the jwt middleware to ensure the user only can get the coordinates of his own device
-	var user_id = res.locals.user_id;
-	db.query('SELECT g.id, g.latitude, g.longtitude, g.time, g.device_id FROM gps AS g, devices AS d WHERE d.user_id = ? AND g.device_id = d.id ORDER BY STR_TO_DATE(g.time, '%Y-%m-%d %HH:%ii') DESC',
-		[user_id],
-		function (error, results, fields) {
-			res.send(results);
-		});
+router.get('/getById', auth.verifyToken, function (req, res) {
+    var id = req.query.id
+    db.query("SELECT * FROM transactions WHERE id=?",
+        [id],
+        function (error, results) {
+            res.send(results);
+        })
+})
+
+router.get('/getToday', auth.verifyToken, function (req, res) {
+    var bank_account = req.body.bank_account
+    db.query("SELECT * FROM transactions WHERE bank_account=? AND DATE_FORMAT(date, '%Y-%m-%d')=CURDATE()",
+        [bank_account],
+        function (error, results) {
+            res.send(results);
+        })
+})
+
+router.get('/getSince', auth.verifyToken, function (req, res) {
+    var bank_account = req.body.bank_account
+    var date = req.query.date
+    db.query("SELECT * FROM transactions WHERE bank_account=? AND DATE_FORMAT(date, '%Y-%m-%d') >= DATE_FORMAT(?, '%Y-%m-%d')",
+        [bank_account, date],
+        function (error, results) {
+            res.send(results);
+        })
 })
 
 router.get('/getBetweenDates', auth.verifyToken, function (req, res) {
-	//Take the user id directly from the jwt middleware to ensure the user only can get the coordinates of his own device
-	var user_id = res.locals.user_id;
+    var bank_account = req.body.bank_account
     var start = req.query.start
     var end = req.query.end
-    db.query("SELECT g.id, g.latitude, g.longtitude, g.time, g.device_id FROM gps AS g, devices AS d WHERE d.user_id = ? AND g.device_id = d.device_id AND DATE_FORMAT(date, '%Y-%m-%d') >= DATE_FORMAT(?, '%Y-%m-%d') AND DATE_FORMAT(date, '%Y-%m-%d') <= DATE_FORMAT(?, '%Y-%m-%d') ORDER BY STR_TO_DATE(g.time, '%Y-%m-%d %HH:%ii') DESC",
+    db.query("SELECT * FROM transactions WHERE bank_account=? AND DATE_FORMAT(date, '%Y-%m-%d') >= DATE_FORMAT(?, '%Y-%m-%d') AND DATE_FORMAT(date, '%Y-%m-%d') <= DATE_FORMAT(?, '%Y-%m-%d')",
         [bank_account, start, end],
         function (error, results) {
             res.send(results);
         })
 })
 
+router.get('/delete', auth.verifyToken, function (req, res) {
+    var id = req.query.id
+    db.query('DELETE from transactions WHERE id=?',
+        [id],
+        function (error, results, fields) {
+            if (results != null && results.affectedRows == 1) {
+                res.json({
+                    success: true
+                })
+            } else {
+                console.log(error)
+                res.json({
+                    success: false
+                })
+            }
+        })
+})
 module.exports = router;
