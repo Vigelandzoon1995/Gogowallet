b9b0cd3520bb717b79c37ae9af05bb4c25efb736
frunkyfrunk <verhagen.frank@gmail.com>
2019-01-17 15:11:12 +0100

    Update solenoid.js

--- routes/solenoid.js
+++ routes/solenoid.js
@@ -2,14 +2,14 @@ var express = require('express');
 var auth = require('../modules/auth');
 var router = express.Router();
 
-router.post('/state', auth.verifyToken, function (req, res){
+router.post('/status/set', auth.verifyToken, function (req, res) {
 	var device_id = req.body.device_id;
 	var user_id = res.locals.user_id
 	var solenoidstate = req.body.solenoidstate;
 
 	db.query("SELECT * FROM devices WHERE id=? AND user_id=?",
 		[device_id, user_id],
-		function(err, result){
+		function (err, result) {
 			db.query("UPDATE devices SET solenoidstate=? WHERE id=?", [solenoidstate, device_id], function (err, result) {
 				if (err) {
 					throw err;
@@ -28,7 +28,24 @@ router.post('/state', auth.verifyToken, function (req, res){
 
 })
 
-router.post('/off', auth.verifyToken, function (req, res){
+router.get('/status/get', auth.verifyToken, function (req, res) {
+	var device_id = req.query.id;
+	var user_id = res.locals.user_id
+
+	db.query("SELECT * FROM devices WHERE id=? AND user_id=?",
+		[device_id, user_id],
+		function (err, result) {
+			if (result) {
+				res.json(result)
+			} else {
+				console.log(error)
+				res.json(false)
+			}
+		})
+
+})
+
+router.post('/off', auth.verifyToken, function (req, res) {
 	res.json({
 		response: 'Status: Off'
 	})
