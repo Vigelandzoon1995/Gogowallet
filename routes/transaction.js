b7364b123a65dc2afcca65c21ca0a5a32e09b0b7
Johan Bos <9801560+JHNBOS@users.noreply.github.com>
2019-01-19 21:45:23 +0100

    Update transaction.js

--- routes/transaction.js
+++ routes/transaction.js
@@ -3,90 +3,108 @@ var auth = require('../modules/auth')
 var router = express.Router();
 
 router.post('/create', auth.verifyToken, function (req, res) {
-    var user_id = res.locals.user_id;
-    var name = req.body.name;
-    var amount = req.body.amount;
-    var date = req.body.date;
-    var bank_account = req.body.bank_account;
-    db.query("INSERT INTO transactions (user_id, name, amount, date, bank_account) VALUES (?,?,?,?,?)",
-        [user_id, name, amount, date, bank_account],
-        function (error, results) {
-            if (results != null && results.affectedRows == 1) {
-                res.json({
-                    success: true
-                });
-            } else {
-                console.log(error);
-                res.json({
-                    success: false
-                });
-            }
-        });
+	var user_id = res.locals.user_id;
+	var name = req.body.name;
+	var amount = req.body.amount;
+	var date = req.body.date;
+	var bank_account = req.body.bank_account;
+
+	db.query("INSERT INTO transactions (user_id, name, amount, date, bank_account) VALUES (?,?,?,?,?)",
+		[user_id, name, amount, date, bank_account],
+		function (error, results) {
+			if (results != null && results.affectedRows == 1) {
+				res.json({
+					success: true
+				});
+			} else {
+				res.json({
+					success: false
+				});
+			}
+		});
 });
 
 router.get('/getById', auth.verifyToken, function (req, res) {
-    var id = req.query.id;
-    db.query("SELECT * FROM transactions WHERE id=?",
-        [id],
-        function (error, results) {
-            res.send(results);
-        });
+	var id = req.query.id;
+
+	db.query("SELECT * FROM transactions WHERE id=?",
+		[id],
+		function (error, results) {
+			res.send(results);
+		});
 });
 
 router.get('/getByBankAccount', auth.verifyToken, function (req, res) {
-    var bank_account = req.query.bank_account;
-    db.query("SELECT * FROM transactions WHERE bank_account=? ORDER BY date DESC",
-        [bank_account],
-        function (error, results) {
-            res.send(results);
-        });
+	var bank_account = req.query.bank_account;
+
+	db.query("SELECT * FROM transactions WHERE bank_account=? ORDER BY date DESC",
+		[bank_account],
+		function (error, results) {
+			res.send(results);
+		});
 });
 
 router.get('/getToday', auth.verifyToken, function (req, res) {
-    var bank_account = req.query.bank_account;
-    db.query("SELECT * FROM transactions WHERE bank_account=? AND DATE_FORMAT(date, '%Y-%m-%d')=CURDATE() ORDER BY date DESC",
-        [bank_account],
-        function (error, results) {
-            res.send(results);
-        });
+	var bank_account = req.query.bank_account;
+
+	db.query("SELECT * FROM transactions WHERE bank_account=? AND DATE_FORMAT(date, '%Y-%m-%d')=CURDATE() ORDER BY date DESC",
+		[bank_account],
+		function (error, results) {
+			res.send(results);
+		});
 });
 
 router.get('/getSince', auth.verifyToken, function (req, res) {
-    var bank_account = req.query.bank_account;
-    var date = req.query.date;
-    db.query("SELECT * FROM transactions WHERE bank_account=? AND DATE_FORMAT(date, '%Y-%m-%d') >= DATE_FORMAT(?, '%Y-%m-%d') ORDER BY date DESC",
-        [bank_account, date],
-        function (error, results) {
-            res.send(results);
-        });
+	var bank_account = req.query.bank_account;
+	var date = req.query.date;
+
+	db.query("SELECT * FROM transactions WHERE bank_account=? AND DATE_FORMAT(date, '%Y-%m-%d') >= DATE_FORMAT(?, '%Y-%m-%d') ORDER BY date DESC",
+		[bank_account, date],
+		function (error, results) {
+			res.send(results);
+		});
 });
 
 router.get('/getBetweenDates', auth.verifyToken, function (req, res) {
-    var bank_account = req.query.bank_account;
-    var start = req.query.start;
-    var end = req.query.end;
-    db.query("SELECT * FROM transactions WHERE bank_account=? AND DATE_FORMAT(date, '%Y-%m-%d') >= DATE_FORMAT(?, '%Y-%m-%d') AND DATE_FORMAT(date, '%Y-%m-%d') <= DATE_FORMAT(?, '%Y-%m-%d') ORDER BY date DESC",
-        [bank_account, start, end],
-        function (error, results) {
-            res.send(results);
-        });
+	var bank_account = req.query.bank_account;
+	var start = req.query.start;
+	var end = req.query.end;
+
+	db.query("SELECT * FROM transactions WHERE bank_account=? AND DATE_FORMAT(date, '%Y-%m-%d') >= DATE_FORMAT(?, '%Y-%m-%d') AND DATE_FORMAT(date, '%Y-%m-%d') <= DATE_FORMAT(?, '%Y-%m-%d') ORDER BY date DESC",
+		[bank_account, start, end],
+		function (error, results) {
+			res.send(results);
+		});
+});
+
+router.get('/getSum', auth.verifyToken, function (req, res) {
+	var bank_account = req.query.bank_account;
+	var start = req.query.start;
+	var end = req.query.end;
+
+	db.query("SELECT SUM(amount) FROM transactions WHERE bank_account=? AND CURRENT_TIMESTAMP >= ? AND CURRENT_TIMESTAMP < ?",
+		[bank_account, start, end],
+		function (error, results) {
+			res.send(results);
+		});
 });
 
 router.get('/delete', auth.verifyToken, function (req, res) {
-    var id = req.query.id;
-    db.query('DELETE from transactions WHERE id=?',
-        [id],
-        function (error, results, fields) {
-            if (results != null && results.affectedRows == 1) {
-                res.json({
-                    success: true
-                });
-            } else {
-                console.log(error);
-                res.json({
-                    success: false
-                });
-            }
-        });
+	var id = req.query.id;
+
+	db.query('DELETE from transactions WHERE id=?',
+		[id],
+		function (error, results, fields) {
+			if (results != null && results.affectedRows == 1) {
+				res.json({
+					success: true
+				});
+			} else {
+				res.json({
+					success: false
+				});
+			}
+		});
 });
+
 module.exports = router;
