c89b56e01cf81f6a29de108a21ab1d8a1119e192
frunkyfrunk <verhagen.frank@gmail.com>
2019-01-22 10:32:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ var https = require('https');
 if (process.argv[1].includes("jasmine.js")) {
   http.createServer(app).listen(4444, "0.0.0.0");
   console.log("running jasmine test environment at port 4444");
-} else if (process.argv[2].includes("--local")) {
+} else if (process.argv[2] != null && process.argv[2].includes("--local")) {
   http.createServer(app).listen(3333, "::");
   console.log("running at port 3333");
 } else {
